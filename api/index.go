258c187a31877ca46a57fe479c3c36ebd2d25005
zach <zachflentgewong@gmail.com>
2024-05-08 14:04:23 -0700

    how about this

--- api/index.go
+++ api/index.go
@@ -14,15 +14,17 @@ var staticFiles embed.FS
 var templates = template.Must(template.ParseFS(staticFiles, "src/templates/*.html"))
 
 func Handler(w http.ResponseWriter, r *http.Request) {
+	fs := http.FileServerFS(staticFiles)
+	fs.ServeHTTP(w, r)
 	templates.ExecuteTemplate(w, "index.html", nil)
 }
 
 func Main() {
 	router := http.NewServeMux()
 
-	router.Handle("GET /src/", http.FileServerFS(staticFiles))
+	// router.Handle("GET /", http.FileServerFS(staticFiles))
 
-	router.HandleFunc("/", Handler)
+	router.HandleFunc("GET /", Handler)
 
 	log.Println("LISTENING AT PORT:8080")
 	log.Fatal(http.ListenAndServe(":8080", router))
