affd4f8c732bdfd3b3c8b603eb31211d7b2fa8fc
zach <zachflentgewong@gmail.com>
2024-05-08 23:21:33 -0700

    added everything to one handler function

--- api/index.go
+++ api/index.go
@@ -5,6 +5,7 @@ package handler
 import (
 	"embed"
 	"html/template"
+	"io"
 	"log"
 	"net/http"
 )
@@ -14,15 +15,36 @@ var staticFiles embed.FS
 var templates = template.Must(template.ParseFS(staticFiles, "src/templates/*.html"))
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	templates.ExecuteTemplate(w, "index.html", nil)
+	switch r.URL.Path {
+	case "/":
+		err := templates.ExecuteTemplate(w, "index.html", nil)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	case "/src/style.css":
+		file, err := staticFiles.Open("src/style.css")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusNotFound)
+			return
+		}
+		defer file.Close()
+
+		w.Header().Set("Content-Type", "text/css")
+		_, err = io.Copy(w, file)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	default:
+		http.NotFound(w, r)
+	}
 }
 
 func Main() {
 	router := http.NewServeMux()
 
-	router.Handle("GET /src/", http.FileServerFS(staticFiles))
-
-	router.HandleFunc("GET /", Handler)
+	router.HandleFunc("/", Handler)
 
 	log.Println("LISTENING AT PORT:8080")
 	log.Fatal(http.ListenAndServe(":8080", router))
