9ab192769a04900b146f7e49c59fbcc91adcb8d7
zach <zachflentgewong@gmail.com>
2024-05-09 12:17:04 -0700

    trying routes with templates

--- api/index.go
+++ api/index.go
@@ -15,6 +15,7 @@ var staticFiles embed.FS
 var templates = template.Must(template.ParseFS(staticFiles, "src/templates/*.html"))
 
 func Handler(w http.ResponseWriter, r *http.Request) {
+	templates.ExecuteTemplate(w, "index.html", nil)
 	fmt.Fprintf(w, "<h1>Route: %s</h1>", r.URL.Path)
 }
 
@@ -22,6 +23,7 @@ func Main() {
 	router := http.NewServeMux()
 
 	router.HandleFunc("GET /", Handler)
+	router.HandleFunc("GET /src/", Handler)
 
 	log.Println("LISTENING AT PORT:8080")
 	log.Fatal(http.ListenAndServe(":8080", router))
